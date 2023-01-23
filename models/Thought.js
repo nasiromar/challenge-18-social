a904cddf3ae9908cc9c0c7062018eb48fab6d91e
Mona Lisa <nasiromar321@gmail.com>
2023-01-23 10:36:55 -0600

    thought

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughttext: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+
+    username: {
+      type: String,
+      required: true,
+
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    
+    reactions: [ reactionSchema ],
+      
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+  
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
+
