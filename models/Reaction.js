e43f85a995a5da99324145615a1f4004a3862f92
Mona Lisa <nasiromar321@gmail.com>
2023-01-23 10:44:39 -0600

    reaction

---
+++ models/Reaction.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+
+    reactionid: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId(),
+    },
+
+    username: {
+      type: String,
+      required: true,
+
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    
+    reactions: [ reactionSchema ],
+      
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+  
+
+module.exports = reactionSchema;
+
