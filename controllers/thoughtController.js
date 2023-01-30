d79564327bac6d0fbe9b1c2087f1e6e46dd9c637
Mona Lisa <nasiromar321@gmail.com>
2023-01-30 10:59:39 -0600

    thoughcontroller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -78,6 +78,48 @@ module.exports = {
           : res.json(thought)
       )
       .catch((err) => res.status(500).json(err));
+
+},
+
+// add a reaction to a thought
+addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) => {
+        if (!thought) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  removeReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) => {
+        if (!thought) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
 };
+
+
+
+
 
